068d7311258a4a6a00ac1973d4487f40460d5746
MatioYoshitoki <liuting_502@126.com>
2022-04-07 23:21:50 +0800

    update: 完成鱼的持久化以及捞鱼的部分功能

--- core/src/main/java/com/rw/random/app/usecase/OutObjectUseCase.kt
+++ core/src/main/java/com/rw/random/app/usecase/OutObjectUseCase.kt
@@ -1,7 +1,9 @@
 package com.rw.random.app.usecase
 
+import com.rw.random.common.constants.BeingStatus
 import com.rw.random.domain.entity.RWZone
 import com.rw.random.domain.entity.obj.Fish
+import com.rw.random.domain.repository.FishRepository
 import com.rw.random.infra.subscription.SubscriptionRegistry
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
@@ -15,14 +17,25 @@ interface OutObjectUseCase {
 
 @Component
 open class OutObjectUseCaseImpl(
-    private val subscriptionRegistry: SubscriptionRegistry,
-    private val zone: RWZone
+    private val fishRepository: FishRepository,
+    private val zone: RWZone,
 ) : OutObjectUseCase {
 
     override fun runCase(fishId: Long): Mono<Long> {
-
-        return zone.getAllObjByType(Fish::class)
+        val fishOption = zone.getAllObjByType(Fish::class)
+            .map { it as Fish }
             .filter { it.id == fishId }
-            .filter {  }
+            .filter { it.status == BeingStatus.ALIVE }
+            .findFirst()
+        return Mono.justOrEmpty(fishOption)
+            .doOnNext {
+                zone.clearObj(it)
+            }
+            .delayUntil {
+                fishRepository.saveOne(it)
+            }
+            .map {
+                it.id
+            }
     }
 }
---
+++ core/src/main/java/com/rw/random/infra/scheduler/AliveFishPersistenceScheduler.kt
@@ -0,0 +1,30 @@
+package com.rw.random.infra.scheduler
+
+import com.rw.random.domain.entity.RWZone
+import com.rw.random.domain.entity.obj.Fish
+import com.rw.random.domain.repository.FishRepository
+import com.rw.random.infra.subscription.SubscriptionRegistry
+import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Flux
+
+@Component
+open class AliveFishPersistenceScheduler(
+    private val subscriptionRegistry: SubscriptionRegistry,
+    private val zone: RWZone,
+    private val fishRepository: FishRepository,
+) {
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    @Scheduled(fixedDelay = 500, initialDelay = 3_000)
+    fun doTask() {
+        log.info("获取存活列表")
+        Flux.fromStream(subscriptionRegistry.findObjByTopic(zone.getZoneTopic(), Fish::class))
+            .filter { it is Fish }
+            .delayUntil {
+                fishRepository.saveOne(it as Fish)
+            }
+            .subscribe()
+    }
+}
